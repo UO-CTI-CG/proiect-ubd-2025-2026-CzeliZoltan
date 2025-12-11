14fc16712c978e3e92fd5ceb9ca4c830c54bce15
CzeliZoltan <161700298+CzeliZoltan@users.noreply.github.com>
2025-12-11 17:36:23 +0200

    Created Login API And User Details Service (#8)

---
+++ Cartify-BE/src/main/java/com/eCommerce/Cartify/controller/AuthController.java
@@ -0,0 +1,63 @@
+package com.eCommerce.Cartify.controller;
+
+import com.eCommerce.Cartify.dto.AuthenticationRequest;
+import com.eCommerce.Cartify.repository.UserRepository;
+import com.eCommerce.Cartify.services.jwt.UserDetailsServiceImpl;
+import com.eCommerce.Cartify.utils.JwtUtil;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import com.eCommerce.Cartify.entity.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+import java.io.IOException;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final JwtUtil jwtUtil;
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+
+    private final UserRepository userRepository;
+
+    @PostMapping("/authenticate")
+    public void createAuthenticationToken(
+            @RequestBody AuthenticationRequest authenticationRequest,
+            HttpServletRequest request,
+            HttpServletResponse response) throws IOException, IOException, JSONException {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+        } catch (BadCredentialsException e) {
+            throw new BadCredentialsException("Incorrect username or password");
+        }
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        Optional<User> optionalUser = userRepository.findFirstByEmail(userDetails.getUsername());
+        final String jwt = jwtUtil.generateToken(userDetails.getUsername());
+
+        if (optionalUser.isPresent()) {
+            response.getWriter().write(
+                    new JSONObject()
+                            .put("userId", optionalUser.get().getId())
+                            .put("role", optionalUser.get().getRole())
+                            .toString()
+            );
+        }
+        response.addHeader(HEADER_STRING, TOKEN_PREFIX + jwt);
+    }
+}
