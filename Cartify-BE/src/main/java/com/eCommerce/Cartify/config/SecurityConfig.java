1c9cb0177e3e9c45d78e49ca6c0fc5e03edbc2bb
Czeli24 <czelizoltandragos@gmail.com>
2025-12-11 17:33:48 +0200

    Created Login API And User Details Service

---
+++ Cartify-BE/src/main/java/com/eCommerce/Cartify/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.eCommerce.Cartify.config;
+
+import com.eCommerce.Cartify.filters.JwtRequestFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtRequestFilter jwtRequestFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/authenticate").permitAll()   // LOGIN PUBLIC
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
---
+++ Cartify-BE/src/main/java/com/eCommerce/Cartify/dto/AuthenticationRequest.java
@@ -0,0 +1,11 @@
+package com.eCommerce.Cartify.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthenticationRequest {
+    private String username;
+    private String password;
+}
+
+
