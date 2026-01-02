090609f331130215faa5c316699723e47089e36b
Numele tau <emailul tau corect>
2026-01-02 12:44:03 +0200

    task: created Signup API, WebSecurity & Admin Account (#62)

---
+++ Cartify-BE/src/main/java/com/eCommerce/Cartify/dto/SignupRequest.java
@@ -0,0 +1,11 @@
+package com.eCommerce.Cartify.dto;
+
+
+import lombok.Data;
+
+@Data
+public class SignupRequest {
+    private String email;
+    private String password;
+    private String name;
+}
